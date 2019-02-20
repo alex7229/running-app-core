bdef9feeea5486b4f50dd3d4887b028b2fe6ee6d
tup1tsa <natorvano@gmail.com>
2019-02-20 16:51:58 +0200

    add all remaining types to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,11 @@
 export * from "./src/application/interfaces";
 export * from "./src/application/testHelpers";
+export * from "./src/application/config";
+
+export * from "./src/application/validators/validateEmail";
+export * from "./src/application/validators/validateLogin";
+export * from "./src/application/validators/validateLoginInfo";
+export * from "./src/application/validators/validatePassword";
+export * from "./src/application/validators/validatePath";
+export * from "./src/application/validators/validateRaces";
+export * from "./src/application/validators/validateUserInfo";
