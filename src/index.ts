c51219f052a73c0fbe9f8b694651a19581d73aa7
tup1tsa <natorvano@gmail.com>
2019-02-20 16:13:52 +0200

    create entry point for npm

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { validateEmail } from "./application/validators/validateEmail";
+export { validateLogin } from "./application/validators/validateLogin";
+export { validateLoginInfo } from "./application/validators/validateLoginInfo";
+export { validatePassword } from "./application/validators/validatePassword";
+export { validatePath } from "./application/validators/validatePath";
+export { validateRaces } from "./application/validators/validateRaces";
+export { validateUserInfo } from "./application/validators/validateUserInfo";
+
+export * from "./application/interfaces";
+export * from "./application/config";
+export * from "./application/testHelpers";
